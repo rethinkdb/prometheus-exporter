7a1b8c4b8e5c034392cc5ab8879b270ac282e986
Alexander Mogilko <cmogilko@gmail.com>
2020-02-29 16:52:56 +0300

    doc files

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+RethinkDB stats exporter provides http-server for Prometheus stats collector.
+*/
+
+// Package main provides binary of the rethinkdb stats prometheus exporter.
+// For building your own binary use this package: github.com/rethinkdb/prometheus-exporter/exporter
+package main
